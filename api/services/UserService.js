dc00da55c518581361105ea5ade9ae4193253aaf
ryan quinn <ryanwilliamquinn@gmail.com>
2015-11-03 11:29:50 -0500

    add an update in there too

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -8,23 +8,28 @@ module.exports = {
         console.log('the user!', user);
         if (!user.hats) return Promise.reject(new Error('no hats for this user: ' + JSON.stringify(user)));
 
-        user.hats.forEach(function(hat) {
-          if (hat.type === 'sombrero') {
-            console.log('in here?', hat.id);
-            user.hats.remove(hat.id);
-            console.log('after removing?', user.hats);
-            user.hats.add({
-              type: 'safari'
-            });
-          }
-        });
+        Promise.each(user.hats, function(hat) {
+            if (hat.type === 'sombrero') {
+              console.log('in here?', hat.id);
+              user.hats.remove(hat.id);
+              console.log('after removing?', user.hats);
+              user.hats.add({
+                type: 'safari'
+              });
+            } else {
+              hat.type = 'used ' + hat.type;
+              return hat.save();
+            }
+          })
+          .then(function() {
 
-        console.log('user.hats?', user.hats);
-        return user.save()
-            .then(function(data) {
+            console.log('user.hats?', user.hats);
+            return user.save()
+              .then(function(data) {
                 console.log('after saving', data);
-            });
+              });
 
+          });
       });
   }
 };
