548fd074ff94b6e3baf065792fe3fe9264bd4582
ryan quinn <ryanwilliamquinn@gmail.com>
2015-11-03 11:25:31 -0500

    add remove example

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -1,20 +1,30 @@
+var Promise = require('bluebird');
+
 module.exports = {
   doNestedUpdate: function(id) {
     User.findOne(id)
-        .populate('hats')
-        .then(function (user) {
-            console.log('the user!', user);
-            if (!user.hats) return Promise.reject(new Error('no hats for this user: ' + JSON.stringify(user)));
+      .populate('hats')
+      .then(function(user) {
+        console.log('the user!', user);
+        if (!user.hats) return Promise.reject(new Error('no hats for this user: ' + JSON.stringify(user)));
 
-            user.hats.forEach(function(hat) {
-                console.log('hat?', hat);
-                hat.type += 'ryan';
-                console.log('updated hat?', hat);
+        user.hats.forEach(function(hat) {
+          if (hat.type === 'sombrero') {
+            console.log('in here?', hat.id);
+            user.hats.remove(hat.id);
+            console.log('after removing?', user.hats);
+            user.hats.add({
+              type: 'safari'
+            });
+          }
+        });
+
+        console.log('user.hats?', user.hats);
+        return user.save()
+            .then(function(data) {
+                console.log('after saving', data);
             });
 
-            console.log('user.hats?', user.hats);
-            return user.save();
-
-        });
+      });
   }
 };
